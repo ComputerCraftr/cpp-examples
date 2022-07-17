fee600b84dba89fa9c51ef23c9371fcf672dbf5b
John Studnicka <contact@zentec.dev>
2022-07-16 19:44:34 -0700

    Add more testing data

--- src/test.cpp
+++ src/test.cpp
@@ -2,8 +2,12 @@
 
 #include <solutions.h>
 
+#include <algorithm>
+#include <cstdint>
 #include <iostream>
+#include <limits>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 std::string VecToString(const std::vector<int>& intVec)
@@ -22,9 +26,30 @@ std::string VecToString(const std::vector<int>& intVec)
     return result;
 }
 
+std::vector<int> GenerateTestVec()
+{
+    constexpr int size = 100;
+    constexpr int range = 100; // Must be at least half of size to produce enough unique values to fill the set
+    std::unordered_set<int> data;
+
+    int randTemp = 0;
+    while (data.size() < size) {
+        randTemp = rand() % (2 * range + 1) - range; // -range to range
+        data.insert(randTemp); // Every inserted int is unique
+    }
+
+    std::vector<int> result(data.begin(), data.end());
+    std::sort(result.begin(), result.end());
+
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
-    constexpr int testCases = 15;
+    const int64_t nTime = time(nullptr);
+    srand(nTime);
+
+    constexpr int testCases = 18;
     std::vector<int> testVecArr[testCases] = {
         {},
         {-3, 0, 2, 34},
@@ -40,8 +65,16 @@ int main(int argc, char *argv[])
         {-2, -1, 1, 4},
         {-30, -12, -11, -4, 7, 8, 37},
         {-30, -12, -11, -4, 1, 3, 6},
-        {-30, -12, -11, -4, -3, 1, 6, 7}
+        {-30, -12, -11, -4, -3, 1, 6, 7},
+        {},
+        GenerateTestVec(),
+        GenerateTestVec()
     };
+
+    for (int i = 0; i <= std::numeric_limits<int8_t>::max(); ++i) {
+        testVecArr[15].push_back(i - 1);
+    }
+    testVecArr[15].push_back(static_cast<int>(std::numeric_limits<int8_t>::max()) + 1);
 
     for (int i = 0; i < testCases; ++i) {
         printf("FirstValueEqualToIndex %s = %i\n", VecToString(testVecArr[i]).c_str(), FirstValueEqualToIndex(testVecArr[i]));
