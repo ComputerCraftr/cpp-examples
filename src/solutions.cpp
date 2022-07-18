b9d3f86fc9338a6edf9472ecca1b6c64b30e1f50
John Studnicka <contact@zentec.dev>
2022-07-18 02:31:26 -0700

    Upload solution 2

--- src/solutions.cpp
+++ src/solutions.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <limits>
+#include <string>
 #include <vector>
 
 // Given an ordered array of distinct integer numbers, return an element whose value is equal to its index, or -1 if none is found with those conditions.
@@ -49,4 +50,19 @@ int FirstValueEqualToIndex(const std::vector<int>& intVec)
     } else {
         return -1;
     }
+}
+
+// Given an input text string and a simple text pattern, find the starting indices of all matches of the pattern in the input text string.
+std::vector<size_t> FindSubStrMatches(const std::string& str, const std::string& subStr)
+{
+    std::vector<size_t> matchingIndices;
+    size_t pos = str.find(subStr);
+
+    while (pos != std::string::npos) {
+        matchingIndices.push_back(pos);
+        ++pos;
+        pos = str.find(subStr, pos);
+    }
+
+    return matchingIndices;
 }
--- src/solutions.h
+++ src/solutions.h
@@ -1,6 +1,10 @@
 // Copyright (c) 2022 John "ComputerCraftr" Studnicka
 
+#include <string>
 #include <vector>
 
 // Given an ordered array of distinct integer numbers, return an element whose value is equal to its index, or -1 if none is found with those conditions.
 int FirstValueEqualToIndex(const std::vector<int>& intVec);
+
+// Given an input text string and a simple text pattern, find the starting indices of all matches of the pattern in the input text string.
+std::vector<size_t> FindSubStrMatches(const std::string& str, const std::string& substr);
